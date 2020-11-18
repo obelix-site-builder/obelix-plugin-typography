1003d8bb4517eadea7223b868fb684b2d6c8d95f
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2020-11-18 08:31:32 -0500

    Resolve themes properly

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+const path = require("path");
 const Typography = require("typography");
 
 const sourceHook = (config) => (siteData) => {
   let theme = {};
   if (config.theme) {
-    theme = require(config.theme);
+    // First look in site node_modules
+    try {
+      theme = require(path.resolve(`./node_modules/${config.theme}`));
+    } catch (error) {
+      theme = require(config.theme);
+    }
   }
+  theme = theme.default || theme;
   for (const key in config) {
     if (key != "theme" && key != "fileName") {
       theme[key] = config[key];
